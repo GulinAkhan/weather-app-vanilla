c3adef75423161236e9f82a2cd636b89657eeead
GulinAkhan <gulin.akhan@outlook.com>
2022-03-22 14:12:31 +0300

    Fixed the JS code

--- src/app.js
+++ src/app.js
@@ -136,22 +136,17 @@ function getPosition(position) {
 }
 
 function showMyTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
   let city = document.querySelector("#city");
-  let currentWeather = document.querySelector("#temperature");
   let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.temp;
-  currentWeather.innerHTML = `${temperature}°C`;
+  celsiusTemperature = Math.round(response.data.main.temp);
   city.innerHTML = `Current Location: ${response.data.name}`;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
